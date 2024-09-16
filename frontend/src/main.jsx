4080995a31dc94dbc365a574ac4a01ab51c22195
Nitesh Kumar Ojha <nitesh303607@gmail.com>
2024-09-16 17:42:28 +0530

    redux - store, authSlice, apiSlice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,4 +2,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+import { store } from "./redux/store";
+
+import { Provider } from "react-redux";
+
+import { Route, ReactProvider, createRoutesFromElements } from "react-router";
+
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />} />)
+);
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <ReactProvider router={router} />
+  </Provider>
+);
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListener } from "@reduxjs/toolkit/query/react";
+
+import authReducer from "./features/auth/authSlice";
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
+
+setupListener(store.dispatch);
+
+export default store;
